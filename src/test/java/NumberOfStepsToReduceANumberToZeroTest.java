63994d7d0bc2b3a16c232d02d49b1f6cb214c111
Swapan Mazumdar <swapanmazumdar@gmail.com>
2024-04-16 00:32:35 +0100

    https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/

---
+++ src/test/java/NumberOfStepsToReduceANumberToZeroTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NumberOfStepsToReduceANumberToZeroTest {
+
+    @Test
+    public void shouldReduceToCorrectNumberOfSteps() {
+        assertEquals(1, new NumberOfStepsToReduceANumberToZero().numberOfSteps(1));
+        assertEquals(2, new NumberOfStepsToReduceANumberToZero().numberOfSteps(2));
+        assertEquals(3, new NumberOfStepsToReduceANumberToZero().numberOfSteps(3));
+        assertEquals(3, new NumberOfStepsToReduceANumberToZero().numberOfSteps(4));
+        assertEquals(6, new NumberOfStepsToReduceANumberToZero().numberOfSteps(14));
+        assertEquals(4, new NumberOfStepsToReduceANumberToZero().numberOfSteps(8));
+        assertEquals(12, new NumberOfStepsToReduceANumberToZero().numberOfSteps(123));
+    }
+}
